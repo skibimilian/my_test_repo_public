e2ee1c9a2b82b30daa8ec7d3958661ca5a47ddce
Stefanos <szczepanszczygielpotrzeszcz@interia.pl>
2024-04-02 21:52:48 +0200

    Print foobar

--- hello.cpp
+++ hello.cpp
@@ -2,10 +2,16 @@
 
 using std::cout;
 
+void print_foobar(void) {
+	cout << "FOOBAR!!!\n"
+}
+
 int main(void) {
 	cout << "hello there" << '\n';
 	for (int i = 0; i < 10; i++)
 		cout << "LINE.\n";
+	
+	print_foobar();
 
 	return 123;
 }
